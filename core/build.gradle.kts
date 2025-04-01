69abd21ca538aad75ede508246ab229678d89906
amalkoott <amalko.ott@gmail.com>
2025-04-01 15:00:34 +0300

    - обновлены зависимости для :core и :app - добавлено разрешение на показ уведомлений

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "ru.amalkoott.example"
+    namespace = "ru.amalkoott.core"
     compileSdk = 34
 
     defaultConfig {
@@ -38,6 +38,9 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.runtime.android)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.activity)
+    implementation(libs.androidx.work.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
